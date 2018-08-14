56d11b3996968f6ae0e646f0b604cc44c3eebe32
Bruno Staub <bruno.staub@gmail.com>
2018-08-14 14:16:04 +0200

    End Unit 28 email password field with material and flex-layout package

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 import { MatIconModule} from '@angular/material';
 
 @NgModule({
-  imports: [ MatButtonModule, MatIconModule ],
-  exports: [ MatButtonModule, MatIconModule ]
+  imports: [ MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ],
+  exports: [ MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule ]
 })
 export class MaterialModule {}
